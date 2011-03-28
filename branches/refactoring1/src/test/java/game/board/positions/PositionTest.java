e833398ac764a1c14c240e18ea5cd075b70a2088
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-03-28 21:03:49 +0000

    Utilisation de l'import statique.

--- branches/refactoring1/src/test/java/game/board/positions/PositionTest.java
+++ branches/refactoring1/src/test/java/game/board/positions/PositionTest.java
@@ -1,13 +1,15 @@
 
 package game.board.positions;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 import org.junit.After;
-import org.junit.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,9 +27,9 @@ public class PositionTest {
     @Test
     public void testNew() {
 
-        Assert.assertEquals(1, this.position.getRow());
-        Assert.assertEquals(2, this.position.getColumn());
-        Assert.assertFalse(this.position.isNull());
+        assertEquals(1, this.position.getRow());
+        assertEquals(2, this.position.getColumn());
+        assertFalse(this.position.isNull());
 
     }
 
@@ -55,37 +57,37 @@ public class PositionTest {
     @Test
     public void testHashCode() {
 
-        Assert.assertEquals(this.position.hashCode(), this.position.hashCode());
-        Assert.assertNotSame(this.position.hashCode(), new Position(1, 1).hashCode());
-        Assert.assertEquals(this.position.hashCode(), new Position(1, 2).hashCode());
+        assertEquals(this.position.hashCode(), this.position.hashCode());
+        assertNotSame(this.position.hashCode(), new Position(1, 1).hashCode());
+        assertEquals(this.position.hashCode(), new Position(1, 2).hashCode());
 
     }
 
     @Test
     public void testEquals() {
 
-        Assert.assertTrue(this.position.equals(this.position));
-        Assert.assertSame(this.position, this.position);
+        assertTrue(this.position.equals(this.position));
+        assertSame(this.position, this.position);
 
-        Assert.assertFalse(this.position.equals(null));
-        Assert.assertFalse(this.position.equals(new Random()));
+        assertFalse(this.position.equals(null));
+        assertFalse(this.position.equals(new Random()));
 
-        Assert.assertFalse(this.position.equals(new Position(1, 3)));
-        Assert.assertFalse(this.position.equals(new Position(2, 2)));
+        assertFalse(this.position.equals(new Position(1, 3)));
+        assertFalse(this.position.equals(new Position(2, 2)));
 
-        Assert.assertTrue(this.position.equals(new Position(1, 2)));
-        Assert.assertNotSame(this.position, new Position(1, 2));
+        assertTrue(this.position.equals(new Position(1, 2)));
+        assertNotSame(this.position, new Position(1, 2));
 
     }
 
     @Test
     public void testCompareTo() {
 
-        Assert.assertEquals(0, this.position.compareTo(this.position));
-        Assert.assertEquals(1, this.position.compareTo(new Position(1, 1)));
-        Assert.assertEquals(-1, this.position.compareTo(new Position(1, 3)));
-        Assert.assertEquals(-1, this.position.compareTo(new Position(2, 2)));
-        Assert.assertEquals(-1, this.position.compareTo(new Position(2, 3)));
+        assertEquals(0, this.position.compareTo(this.position));
+        assertEquals(1, this.position.compareTo(new Position(1, 1)));
+        assertEquals(-1, this.position.compareTo(new Position(1, 3)));
+        assertEquals(-1, this.position.compareTo(new Position(2, 2)));
+        assertEquals(-1, this.position.compareTo(new Position(2, 3)));
 
     }
 
@@ -103,8 +105,8 @@ public class PositionTest {
         positions.add(new Position(1, 3));
         positions.add(new Position(1, 2));
 
-        Assert.assertEquals(new Position(1, 1), Collections.min(positions));
-        Assert.assertEquals(new Position(2, 4), Collections.max(positions));
+        assertEquals(new Position(1, 1), Collections.min(positions));
+        assertEquals(new Position(2, 4), Collections.max(positions));
 
     }
 
