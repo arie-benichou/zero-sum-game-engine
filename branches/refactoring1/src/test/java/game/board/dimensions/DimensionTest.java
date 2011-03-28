863a6a3605bc2cc5a7efa0ca8f0cbc47397898a4
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-03-28 21:56:02 +0000

    Utilisation de l'import statique. Ajout d'un fail en attendant que le TODO soit fait.

--- branches/refactoring1/src/test/java/game/board/dimensions/DimensionTest.java
+++ branches/refactoring1/src/test/java/game/board/dimensions/DimensionTest.java
@@ -1,135 +1,146 @@
-/**
- * 
- */
 
 package game.board.dimensions;
 
+import static game.board.dimensions.Dimensions.Factory.*;
+
+import static org.junit.Assert.*;
+
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * @author artefact
- * 
- */
 public class DimensionTest {
 
     private Dimensions.Interface dimension;
 
-    /**
-     * @throws java.lang.Exception
-     */
     @Before
     public void setUp() throws Exception {
-        this.dimension = Dimensions.Factory.Dimension(3, 3);
+        
+        this.dimension = Dimension(3, 3); //TODO IllegalDimensionException
+        
+        fail("TODO IllegalDimensionException");
+        
     }
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-        this.dimension = null;
-    }
-
-    /**
-     * Test method for
-     * {@link game.board.dimensions.Dimensions#Dimensions(game.board.dimensions.RowsRange, game.board.dimensions.ColumnsRange)}
-     * .
-     */
     @Test
-    public final void testDimensions() {
-        Assert.assertTrue(this.dimension instanceof Dimensions.Interface);
+    public final void testLegalDimensions() {
+        
+        assertTrue(this.dimension instanceof Dimensions.Interface);
+        
     }
-
-    /**
-     * Test method for
-     * {@link game.board.dimensions.Dimensions#lowerBoundForRows()}.
-     */
+    
     @Test
-    public final void testGetRowsLowerBound() {
-        Assert.assertTrue(this.dimension.lowerBoundForRows() == 1);
+    public final void testIllegalDimensions1() {
+        
+        Dimension(0, 0);
+        
     }
-
-    /**
-     * Test method for
-     * {@link game.board.dimensions.Dimensions#upperBoundForRows()}.
-     */
+    
     @Test
-    public final void testGetRowsUpperBound() {
-        Assert.assertTrue(this.dimension.upperBoundForRows() == 3);
+    public final void testIllegalDimensions2() {
+        
+        Dimension(0, 1);
+        
     }
-
-    /**
-     * Test method for
-     * {@link game.board.dimensions.Dimensions#getMinColumnIndex()}.
-     */
+    
     @Test
-    public final void testGetMinColumnIndex() {
-        Assert.assertTrue(this.dimension.lowerBoundForColumns() == 1);
+    public final void testIllegalDimensions3() {
+        
+        Dimension(1, 0);
+        
     }
-
-    /**
-     * Test method for
-     * {@link game.board.dimensions.Dimensions#getMaxColumnIndex()}.
-     */
+    
+    
     @Test
-    public final void testGetMaxColumnIndex() {
-        Assert.assertTrue(this.dimension.upperBoundForColumns() == 3);
+    public final void testIllegalDimensions4() {
+        
+        Dimension(-1, 1);
+        
     }
-
-    /**
-     * Test method for
-     * {@link game.board.dimensions.Dimensions#getRowIndexRangeSize()}.
-     */
+    
     @Test
-    public final void testGetRowIndexRangeSize() {
-        Assert.assertTrue(this.dimension.numberOfRows() == 3);
-    }
+    public final void testIllegalDimensions5() {
+        
+        Dimension(1, -1);
+        
+    }                
+    
 
-    /**
-     * Test method for
-     * {@link game.board.dimensions.Dimensions#getColumnIndexRangeSize()}.
-     */
     @Test
-    public final void testGetColumnIndexRangeSize() {
-        Assert.assertTrue(this.dimension.numberOfColumns() == 3);
+    public final void testLowerBoundForRows() {
+        
+        assertTrue(this.dimension.lowerBoundForRows() == 1);
+        
     }
 
-    /**
-     * Test method for {@link game.board.dimensions.Dimensions#boardCapacity()}.
-     */
     @Test
-    public final void testGetCapacity() {
-        Assert.assertTrue(this.dimension.boardCapacity() == 3 * 3);
+    public final void testUpperBoundForRows() {
+        
+        assertTrue(this.dimension.upperBoundForRows() == 3);
+        
     }
 
-    /**
-     * Test method for
-     * {@link game.board.dimensions.Dimensions#contains(int, int)}.
-     */
+    @Test
+    public final void testLowerBoundForColumns() {
+        
+        assertTrue(this.dimension.lowerBoundForColumns() == 1);
+        
+    }
+
+    @Test
+    public final void testUpperBoundForColumns() {
+        
+        assertTrue(this.dimension.upperBoundForColumns() == 3);
+        
+    }
+
+    @Test
+    public final void testNumberOfRows() {
+        
+        assertTrue(this.dimension.numberOfRows() == 3);
+        
+    }
+
+    @Test
+    public final void testNumberOfColumns() {
+        
+        assertTrue(this.dimension.numberOfColumns() == 3);
+        
+    }
+
+    @Test
+    public final void testBoardCapacity() {
+        
+        assertTrue(this.dimension.boardCapacity() == 3 * 3);
+        
+    }
+
     @Test
     public final void testContains() {
 
-        Assert.assertFalse(this.dimension.contains(0, 1));
-        Assert.assertFalse(this.dimension.contains(1, 0));
+        assertFalse(this.dimension.contains(0, 1));
+        assertFalse(this.dimension.contains(1, 0));
 
-        Assert.assertTrue(this.dimension.contains(1, 1));
-        Assert.assertTrue(this.dimension.contains(1, 2));
-        Assert.assertTrue(this.dimension.contains(1, 3));
+        assertTrue(this.dimension.contains(1, 1));
+        assertTrue(this.dimension.contains(1, 2));
+        assertTrue(this.dimension.contains(1, 3));
 
-        Assert.assertTrue(this.dimension.contains(2, 1));
-        Assert.assertTrue(this.dimension.contains(2, 2));
-        Assert.assertTrue(this.dimension.contains(2, 3));
+        assertTrue(this.dimension.contains(2, 1));
+        assertTrue(this.dimension.contains(2, 2));
+        assertTrue(this.dimension.contains(2, 3));
 
-        Assert.assertTrue(this.dimension.contains(3, 1));
-        Assert.assertTrue(this.dimension.contains(3, 2));
-        Assert.assertTrue(this.dimension.contains(3, 3));
+        assertTrue(this.dimension.contains(3, 1));
+        assertTrue(this.dimension.contains(3, 2));
+        assertTrue(this.dimension.contains(3, 3));
 
-        Assert.assertFalse(this.dimension.contains(3, 4));
-        Assert.assertFalse(this.dimension.contains(4, 1));
+        assertFalse(this.dimension.contains(3, 4));
+        assertFalse(this.dimension.contains(4, 1));
 
     }
+    
+    @After
+    public void tearDown() throws Exception {
+        this.dimension = null;
+    }    
 
 }
