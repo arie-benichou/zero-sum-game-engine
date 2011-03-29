335043a4e8665db3adc35ce8cc9f92556bdf4d69
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-03-29 23:35:10 +0000

    Override des méthodes equals() et hashcode().

--- src/test/java/game/board/BoardTest.java
+++ src/test/java/game/board/BoardTest.java
@@ -10,8 +10,7 @@ import static game.position.API.PositionFactory.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import game.cell.API.CellInterface;
+import java.util.Random;
 
 import static org.junit.Assert.*;
 
@@ -46,6 +45,50 @@ public class BoardTest {
         assertFalse(this.board.getCell(1, 2) == (Cell(Position(1,2))));
         
     }
+    
+    
+    @Test
+    public void testHashCode() {
+
+        assertEquals(this.board.hashCode(), this.board.hashCode());
+        assertNotSame(this.board.hashCode(), Board(2, 3).hashCode());
+        assertEquals(this.board.hashCode(), Board(2, 3).hashCode());
+
+    }
+
+    @Test
+    public void testEquals() {
+
+        assertTrue(this.board.equals(this.board));
+        assertSame(this.board, this.board);
+
+        assertFalse(this.board.equals(null));
+        assertFalse(this.board.equals(new Random()));
+
+        assertFalse(this.board.equals(Board(3, 2)));
+        assertFalse(this.board.equals(Board(2, 2)));
+        
+        assertTrue(this.board.equals(Board(2, 3)));
+        assertNotSame(this.board, Board(2, 3));
+        
+        List<CellInterface> cells = new ArrayList<CellInterface>(3);
+        cells.add(Cell(Position(1, 1)));
+        cells.add(Cell(Position(1, 2)));
+        cells.add(Cell(Position(1, 3)));
+        
+        Board board1 = new Board(cells);
+        Board board2 = new Board(cells);
+        
+        assertTrue(board1.equals(board2));
+        assertNotSame(board1, board2);
+        
+        for(CellInterface cell: board1) {
+            assertSame(cell, board2.getCell(cell.getRow(), cell.getColumn()));
+            assertEquals(cell, board2.getCell(cell.getRow(), cell.getColumn()));
+        }
+
+    }    
+    
 
     @Test
     public final void testIterator() {
