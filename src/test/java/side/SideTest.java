7ed1506afb90e76dc507e1756ea32bc80c135c14
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-03-31 09:13:44 +0000

    Implémentation des tests unitaires de la classe Side.

--- src/test/java/side/SideTest.java
+++ src/test/java/side/SideTest.java
@@ -1,48 +1,61 @@
 package side;
 
+import static side.API.*;
 import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class SideTest {
 
-    @Before
-    public void setUp() throws Exception {}
+	private SideInterface side;
 
-    @After
-    public void tearDown() throws Exception {}
+	@Before
+	public void setUp() {
+		this.side = Side.FIRST_SIDE;
+	}
 
-    @Test
-    public final void testIsFirstSide() {
-        fail("Not yet implemented"); // TODO
-    }
+	@Test
+	public final void testIsFirstSide() {
+		assertTrue(this.side.isFirstSide());
+		assertFalse(Side.SECOND_SIDE.isFirstSide());
+	}
 
-    @Test
-    public final void testIsSecondSide() {
-        fail("Not yet implemented"); // TODO
-    }
+	@Test
+	public final void testIsSecondSide() {
+		assertTrue(Side.SECOND_SIDE.isSecondSide());
+		assertFalse(Side.SECOND_SIDE.isFirstSide());
+	}
 
-    @Test
-    public final void testIsOneSide() {
-        fail("Not yet implemented"); // TODO
-    }
+	@Test
+	public final void testIsOneSide() {
+		assertTrue(this.side.isOneSide());
+		assertTrue(Side.SECOND_SIDE.isOneSide());
+		assertFalse(Side.NULL_SIDE.isOneSide());
+	}
 
-    @Test
-    public final void testIsNull() {
-        fail("Not yet implemented"); // TODO
-    }
+	@Test
+	public final void testIsNull() {
+		assertTrue(Side.NULL_SIDE.isNull());
+		assertFalse(this.side.isNull());
+	}
 
-    @Test
-    public final void testGetNextSide() {
-        fail("Not yet implemented"); // TODO
-    }
+	@Test
+	public final void testGetNextSide() {
+		assertTrue(Side.SECOND_SIDE == this.side.getNextSide());
+		assertTrue(this.side == Side.SECOND_SIDE.getNextSide());
+	}
 
-    @Test
-    public final void testGetNegation() {
-        fail("Not yet implemented"); // TODO
-    }
+	@Test
+	public final void testGetNegation() {
+		assertTrue(Side.NOT_FIRST_SIDE == this.side.getNegation());
+		assertTrue(Side.NOT_SECOND_SIDE == Side.SECOND_SIDE.getNegation());
+	}
+
+	@After
+	public void tearDown() {
+		this.side = null;
+	}
 
 }
