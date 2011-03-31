c3ec830c47084a2d9142d84fbe69e2769023649a
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-03-31 09:45:05 +0000

    Fin d'implémentation des tests unitaires de la classe Side et de l'API.

--- branches/refactoring1/src/test/java/side/APITest.java
+++ branches/refactoring1/src/test/java/side/APITest.java
@@ -1,6 +1,54 @@
-
 package side;
 
+import static side.API.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class APITest {
-    //TODO
+
+	@Test
+	public final void testIsNull() {
+		assertFalse(isNull(FIRST_SIDE));
+		assertFalse(isNull(SECOND_SIDE));
+		assertTrue(isNull(NULL_SIDE));
+	}
+
+	@Test
+	public final void testIsFirstSide() {
+		assertTrue(isFirstSide(FIRST_SIDE));
+		assertFalse(isFirstSide(SECOND_SIDE));
+		assertFalse(isFirstSide(NULL_SIDE));
+	}
+
+	@Test
+	public final void testIsSecondSide() {
+		assertTrue(isSecondSide(SECOND_SIDE));
+		assertFalse(isSecondSide(FIRST_SIDE));
+		assertFalse(isSecondSide(NULL_SIDE));
+	}
+
+	@Test
+	public final void testIsOneSide() {
+		assertTrue(isOneSide(FIRST_SIDE));
+		assertTrue(isOneSide(SECOND_SIDE));
+		assertFalse(isOneSide(NULL_SIDE));
+	}
+
+	@Test
+	public final void testNext() {
+		assertEquals(next(FIRST_SIDE), SECOND_SIDE);
+		assertEquals(next(SECOND_SIDE), FIRST_SIDE);
+		assertEquals(next(NULL_SIDE), NULL_SIDE);
+		assertEquals(next(FIRST_SIDE.getNegation()), FIRST_SIDE.getNegation());
+		assertEquals(next(SECOND_SIDE.getNegation()), SECOND_SIDE.getNegation());
+	}
+
+	@Test
+	public final void testNot() {
+		assertEquals(not(FIRST_SIDE), FIRST_SIDE.getNegation());
+		assertEquals(not(SECOND_SIDE), SECOND_SIDE.getNegation());
+		assertEquals(not(NULL_SIDE), NULL_SIDE);
+	}
+
 }
