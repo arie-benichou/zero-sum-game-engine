1b6e9d14ab95a04fa8cb8eb8500e3bce6b09c0c7
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-03-31 07:56:07 +0000

    Squelette de tests pour Side.

--- branches/refactoring1/src/test/java/side/SideTest.java
+++ branches/refactoring1/src/test/java/side/SideTest.java
@@ -1,6 +1,48 @@
-
 package side;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class SideTest {
-    //TODO
+
+    @Before
+    public void setUp() throws Exception {}
+
+    @After
+    public void tearDown() throws Exception {}
+
+    @Test
+    public final void testIsFirstSide() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testIsSecondSide() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testIsOneSide() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testIsNull() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testGetNextSide() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public final void testGetNegation() {
+        fail("Not yet implemented"); // TODO
+    }
+
 }
