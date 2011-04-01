f60b756616084a2fe5fb514949622ca069c972d9
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-04-01 20:16:01 +0000

    Ajout de la partie concernant un jeu.

---
+++ branches/refactoring1/src/main/java/game/GameInterface.java
@@ -0,0 +1,157 @@
+
+package game;
+
+import java.util.List;
+
+import move.API.MoveInterface;
+import side.API.SideInterface;
+import cell.API.CellInterface;
+import position.API.PositionInterface;
+
+/**
+ * This is the interface for a game.
+ */
+public interface GameInterface {
+
+    /**
+     * Returns true if this game allows null move, false otherwise.
+     * 
+     * @return true if this game allows null move, false otherwise
+     */
+    boolean hasNullMove();
+
+    /**
+     * Returns the list of legal moves for a given side.
+     * 
+     * @param side
+     *            a given side
+     * 
+     * @return the list of legal moves for a given side
+     */
+    List<MoveInterface> getLegalMoves(final SideInterface side);
+
+    /**
+     * Plays a given move and returns the next side to play.
+     * 
+     * @param move
+     *            a given move
+     * 
+     * @return the next side to play
+     */
+    SideInterface play(final MoveInterface move);
+
+    /**
+     * Applies a given move and returns true if the move is completed, false
+     * otherwise.
+     * 
+     * @param move
+     *            a given move
+     * 
+     * @return true if the move is completed, false otherwise
+     */
+    boolean doMove(final MoveInterface move);
+
+    /**
+     * Undo a given move and returns true if the move is completely undone,
+     * false otherwise.
+     * 
+     * @param move
+     *            the played move
+     * 
+     * @return true if the move is completely undone, false otherwise
+     */
+    boolean undoMove(final MoveInterface move);
+
+    /**
+     * Returns true if the game is over from a victory, false otherwise.
+     * 
+     * @param move
+     *            the just played move
+     * 
+     * @return true if the game is over from a victory, false otherwise
+     */
+    boolean isGameOverFromVictory(final MoveInterface move);
+
+    /**
+     * Returns true if the game is over from a draw, false otherwise.
+     * 
+     * @param move
+     *            the just played move
+     * 
+     * @return true if the game is over from a victory of the current player,
+     *         false otherwise
+     */
+    boolean isGameOverFromDraw(final MoveInterface move);
+
+    /**
+     * Returns FIRST_PLAYER, if it's first player's turn, SECOND_PLAYER, if it's
+     * second player's turn, NOT_FIRST_PLAYER if the second player is a winner,
+     * NOT_SECOND_PLAYER, if the first player is a winner, NO_ONE, if the game
+     * is a draw.
+     * 
+     * @param move
+     *            the just played move
+     * 
+     * @param isMoveDone
+     *            the boolean for the move completion
+     * 
+     * @return FIRST_PLAYER, if it's first player's turn, SECOND_PLAYER, if it's
+     *         second player's turn, NOT_FIRST_PLAYER if the second player is a
+     *         winner, NOT_SECOND_PLAYER, if the first player is a winner,
+     *         NO_ONE, if the game is a draw
+     */
+    SideInterface getNextSideToPlay(final MoveInterface move, final boolean isMoveDone);
+
+    /**
+     * Returns the computation of the static evaluation for a given move.
+     * 
+     * @param move
+     *            a given move
+     * 
+     * @return the computation of the static evaluation for a given move
+     */
+    //double computeStaticEvaluation(final MoveInterface move);
+
+    // ---------------------------------------------------------------------
+    // Façades
+    // ---------------------------------------------------------------------
+
+    /**
+     * Returns a piece of this game for a given player and a given type of
+     * piece.
+     * 
+     * @param player
+     *            a given player
+     * 
+     * @param pieceType
+     *            a given type of piece.
+     * 
+     * @return a piece of this game for a given player and a given type of piece
+     */
+    //PieceInterface piece(final SideInterface player,
+    //        final PieceTypeInterface pieceType);
+
+    /**
+     * Returns the board cell related to a given position.
+     * 
+     * @param position
+     *            a given position
+     * 
+     * @return the board cell related to a given position
+     */
+    //CellInterface cell(final PositionInterface position);
+
+    /**
+     * Returns the board cell related to a given row and a given column.
+     * 
+     * @param clientRowIndex
+     *            a given row
+     * 
+     * @param clientColumnIndex
+     *            a given column
+     * 
+     * @return the board cell related to a given row and a given column
+     */
+    CellInterface cell(final int clientRowIndex, final int clientColumnIndex);
+
+}
--- branches/refactoring1/src/main/java/opponent/Opponent.java
+++ branches/refactoring1/src/main/java/opponent/Opponent.java
@@ -1,37 +1,36 @@
+
 package opponent;
 
 import side.API.SideInterface;
 
 class Opponent implements OpponentInterface {
 
-	private final String name;
-	private final SideInterface side;
-	private final StrategyInterface strategy;
-	private final GameInterface context;
-	
-	
+    private final String name;
+    private final SideInterface side;
+    private final StrategyInterface strategy;
+    private final GameInterface context;
 
-	public Opponent(final String name, final SideInterface side /*, final GameInterface context, final StrategyInterface strategy*/) {
-		this.name = name;
-		this.side = side;
-		this.strategy = strategy;
-		this.context = context;
-	}
+    public Opponent(final String name, final SideInterface side /*, final GameInterface context, final StrategyInterface strategy*/) {
+        this.name = name;
+        this.side = side;
+        this.strategy = strategy;
+        this.context = context;
+    }
 
-	public final String getName() {
-		return this.name;
-	}
+    public final String getName() {
+        return this.name;
+    }
 
-	public final SideInterface getSide() {
-		return this.side;
-	}
-	
-	public final StrategyInterface getStrategy() {
-		return this.strategy;
-	}
-	
-	public final GameInterface getContext() {
-		return this.context;		
-	}			
-	
+    public final SideInterface getSide() {
+        return this.side;
+    }
+
+    public final StrategyInterface getStrategy() {
+        return this.strategy;
+    }
+
+    public final GameInterface getContext() {
+        return this.context;
+    }
+
 }
