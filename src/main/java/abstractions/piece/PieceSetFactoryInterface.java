72e15005aa2babb07b850ab33268416fb8285a9d
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-04-09 17:29:02 +0000

    PositionManager.java: Ajout de la méthode privée initializeData(). Classe déclarée finale.
    PieceSetFactoryInterface.java:
    Visibilité par défaut (package).

--- src/main/java/abstractions/piece/PieceSetFactoryInterface.java
+++ src/main/java/abstractions/piece/PieceSetFactoryInterface.java
@@ -19,7 +19,7 @@ package abstractions.piece;
 
 import java.util.Set;
 
-public interface PieceSetFactoryInterface {
+interface PieceSetFactoryInterface {
 
     public <T extends Enum<T> & PieceTypeInterface> Set<PieceInterface> newPieceSet(final Class<T> piecesSet);
 
