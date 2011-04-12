7f2979b6723b0754ee1a3b4b9652f0e908d08036
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-04-12 19:40:37 +0000

    Avancement dans le DirectionManager.

--- branches/refactoring3/src/main/java/abstractions/direction/DirectionManager.java
+++ branches/refactoring3/src/main/java/abstractions/direction/DirectionManager.java
@@ -1,6 +1,7 @@
 
 package abstractions.direction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import abstractions.dimension.API.DimensionFactory;
@@ -13,7 +14,7 @@ public class DirectionManager implements DirectionManagerInterface {
     private final DimensionInterface dimension;
     private final int hashFactor;
     private final int hashOffset;
-    private final List<DirectionInterface> data = null;
+    private final List<DirectionInterface> data;
 
     private DirectionInterface newDirection(final int rowDelta, final int columnDelta) {
         return new Direction(rowDelta, columnDelta);
@@ -24,63 +25,35 @@ public class DirectionManager implements DirectionManagerInterface {
     }
 
     private int computeHashOffset(final DimensionInterface dimension) {
-        //((dimension.numberOfRows() - 1) + (dimension.numberOfRows() - 0)) * ((dimension.numberOfColumns() - 1) + (dimension.numberOfColumns() - 0))
-        final int n = (2 * dimension.numberOfRows() - 1) * (2 * dimension.numberOfColumns() - 1);
-        return n / 2;
+        //((dimension.numberOfRows() - 1) + (dimension.numberOfRows() - 0)) * ((dimension.numberOfColumns() - 1) + (dimension.numberOfColumns() - 0)) / 2
+        return (2 * dimension.numberOfRows() - 1) * (2 * dimension.numberOfColumns() - 1) / 2;
     }
 
     private int computeNaturalHash(final int rowDelta, final int columnDelta) {
         return this.hashOffset + this.hashFactor * rowDelta + columnDelta;
-
     }
 
-    private void manage(final DirectionInterface direction) {
-        //this.data.add(this.computeNaturalHash(direction.getRowDelta(), direction.getColumnDelta()), direction);
-
-        final int index = this.computeNaturalHash(direction.getRowDelta(), direction.getColumnDelta());
-
-        System.out.println(index);
-
-        this.data.set(index, direction);
-    }
-
-    public DirectionManager(final DimensionInterface dimension) throws Exception {
-
-        this.dimension = dimension;
-
-        this.hashFactor = this.computeHashFactor(this.dimension);
-        System.out.println("hashFactor: " + this.hashFactor);
-
-        this.hashOffset = this.computeHashOffset(this.dimension);
-        System.out.println("size of set: " + this.hashOffset * 2);
-
-        System.out.println("hashOffset: " + this.hashOffset);
-
-        System.out.println("\n");
-
-        /*
-        this.data = Lists.newArrayListWithCapacity(2 * this.hashOffset + 1);
-        Collections.fill(this.data, DirectionManager.NULL_DIRECTION);
-        */
-
+    private List<DirectionInterface> initializeData(final List<DirectionInterface> data) {
         final int maxRowDelta = this.dimension.numberOfRows() - 1;
         final int maxColumnDelta = this.dimension.numberOfColumns() - 1;
-
-        int n = 0;
-        for (int rowDelta = -maxRowDelta; rowDelta <= maxRowDelta; ++rowDelta) {
+        for (int checkSum = 0, rowDelta = -maxRowDelta; rowDelta <= maxRowDelta; ++rowDelta) {
             for (int columnDelta = -maxColumnDelta; columnDelta <= maxColumnDelta; ++columnDelta) {
-                final int hash = this.computeNaturalHash(rowDelta, columnDelta);
-                System.out.println(rowDelta + " | " + columnDelta);
-                System.out.println(n);
-                System.out.println("natural hash : " + hash);
-                if (n != hash) {
-                    throw new Exception("fuck");
+                final DirectionInterface managedDirection = this.newDirection(rowDelta, columnDelta);
+                final int hash = this.computeNaturalHash(managedDirection.getRowDelta(), managedDirection.getColumnDelta());
+                if (hash != checkSum++) {
+                    throw new RuntimeException("Error in hash function.");
                 }
-                System.out.println("\n");
-                ++n;
+                data.add(hash, managedDirection);
             }
         }
+        return data;
+    }
 
+    public DirectionManager(final DimensionInterface dimension) {
+        this.dimension = dimension;
+        this.hashFactor = this.computeHashFactor(this.dimension);
+        this.hashOffset = this.computeHashOffset(this.dimension);
+        this.data = this.initializeData(new ArrayList<DirectionInterface>(2 * this.hashOffset + 1));
     }
 
     public DirectionInterface getDirection(final NamedDirection label) {
@@ -92,9 +65,20 @@ public class DirectionManager implements DirectionManagerInterface {
         return null;
     }
 
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) {
+
         new DirectionManager(DimensionFactory.Dimension(4, 5));
         new DirectionManager(DimensionFactory.Dimension(3, 3));
-        new DirectionManager(DimensionFactory.Dimension(5, 4));
+        final DirectionManagerInterface manager = new DirectionManager(DimensionFactory.Dimension(5, 4));
+
+        System.out.println(manager.getDirection(NamedDirection.TOP));
+        System.out.println(manager.getDirection(NamedDirection.TOP_RIGHT));
+        System.out.println(manager.getDirection(NamedDirection.RIGHT));
+        System.out.println(manager.getDirection(NamedDirection.BOTTOM_RIGHT));
+        System.out.println(manager.getDirection(NamedDirection.BOTTOM));
+        System.out.println(manager.getDirection(NamedDirection.BOTTOM_LEFT));
+        System.out.println(manager.getDirection(NamedDirection.LEFT));
+        System.out.println(manager.getDirection(NamedDirection.TOP_LEFT));
+
     }
 }
