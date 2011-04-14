fdffbfd37a84416ddcade04f69c16106fdf8d76e
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-04-14 23:23:32 +0000

    Fin de l'itération "refactoring3".

--- src/main/java/concretisations/checkers/mutations/CheckersMutation.java
+++ src/main/java/concretisations/checkers/mutations/CheckersMutation.java
@@ -30,4 +30,47 @@ public abstract class CheckersMutation extends AbstractCompositeMutation impleme
         return super.toString() + " | " + this.getDirection();
     }
 
+    @Override
+    public int hashCode() {
+
+        int hashCode = 17;
+
+        hashCode *= 31;
+        hashCode += this.getType().hashCode();
+
+        hashCode *= 31;
+        hashCode += this.getCell().hashCode();
+
+        hashCode *= 31;
+        hashCode += this.getDirection().hashCode();
+
+        return hashCode;
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        final boolean isEqual;
+        if (object == this) {
+            isEqual = true;
+        }
+        else if (object == null) {
+            isEqual = false;
+        }
+        else if (!(object instanceof CheckersMutation)) {
+            isEqual = false;
+        }
+        else {
+            // TODO ! revoir la méthode equals() d'une cellule
+            final CheckersMutation that = (CheckersMutation) object;        
+            isEqual =
+                    that.getType().equals(this.getType())
+                            &&
+                            that.getDirection().getRowDelta() == this.getDirection().getRowDelta()
+                            &&
+                            that.getDirection().getColumnDelta() == this.getDirection().getColumnDelta()
+                            &&
+                            that.getCell().getPosition().equals(this.getCell().getPosition());
+        }
+        return isEqual;
+    }
 }
