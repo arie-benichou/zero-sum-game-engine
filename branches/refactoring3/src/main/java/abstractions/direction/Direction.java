9e1ec8144558bfa00bd0a09abf237ecde667f757
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-04-14 09:46:06 +0000

    Finalisation du package "direction".

--- branches/refactoring3/src/main/java/abstractions/direction/Direction.java
+++ branches/refactoring3/src/main/java/abstractions/direction/Direction.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Arie Benichou
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package abstractions.direction;
 
@@ -22,6 +38,26 @@ public final class Direction implements DirectionInterface {
     @Override
     public final String toString() {
         return "[rowDelta = " + this.getRowDelta() + "]" + "[columnDelta = " + this.getColumnDelta() + "]";
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        final boolean isEqual;
+
+        if (object == this) {
+            isEqual = true;
+        }
+        else if (object == null) {
+            isEqual = false;
+        }
+        else if (!(object instanceof DirectionInterface)) {
+            isEqual = false;
+        }
+        else {
+            final DirectionInterface that = (DirectionInterface) object;
+            isEqual = that.getRowDelta() == this.getRowDelta() && that.getColumnDelta() == this.getColumnDelta();
+        }
+        return isEqual;
     }
 
 }
--- branches/refactoring3/src/main/java/abstractions/direction/DirectionManagerInterface.java
+++ branches/refactoring3/src/main/java/abstractions/direction/DirectionManagerInterface.java
@@ -1,17 +1,35 @@
+/*
+ * Copyright 2011 Arie Benichou
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package abstractions.direction;
 
 import java.util.Collection;
-import java.util.Map;
+import java.util.List;
+
+import abstractions.direction.DirectionManager.NamedDirection;
 
 public interface DirectionManagerInterface {
 
-    DirectionInterface getNamedDirectionFromList(final NamedDirection label);
+    DirectionInterface getNamedDirection(final NamedDirection label);
 
     DirectionInterface getDirection(final int rowDelta, final int columnDelta);
 
-    DirectionInterface reduce(final Collection<DirectionInterface> directions);
+    DirectionInterface reduce(final Collection<? extends DirectionInterface> directions);
 
-    Map<NamedDirection, DirectionInterface> getDirectionsMap();
+    List<NamedDirection> getNamedDirections();
 
 }
