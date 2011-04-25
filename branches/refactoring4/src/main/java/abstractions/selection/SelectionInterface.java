a3e0825d5d532a369d60e4190026b347cc970edc
arie.benichou <arie.benichou@e4ab807e-7155-c4f5-4547-f03407b5bf5c>
2011-04-25 01:37:44 +0000

    Commit de sauvegarde.

---
+++ branches/refactoring4/src/main/java/abstractions/selection/SelectionInterface.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2011 Arie Benichou
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package abstractions.selection;
+
+import java.util.List;
+
+import abstractions.mutation.MutationInterface;
+
+public interface SelectionInterface {
+
+    List<MutationInterface> applySelection(final List<MutationInterface> mutations);
+
+    //void setContext(final GameInterface context);
+
+    //GameInterface getContext();
+
+}
